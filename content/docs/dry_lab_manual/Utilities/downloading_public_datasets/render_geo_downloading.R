e294c3066694923c07b6bea88d02bd34c00d2f44
arc85 <arc85@pitt.edu>
2023-10-26 11:21:42 -0400

    render geo download

---
+++ content/docs/dry_lab_manual/Utilities/downloading_public_datasets/render_geo_downloading.R
@@ -0,0 +1,5 @@
+library(rmarkdown)
+
+render(input="/Users/ARC85/cillo_lab_materials/lab_manuals/dry_lab_manual/Cillo_Dry_Lab_Manual/content/docs/dry_lab_manual/Utilities/downloading_public_datasets/geo_downloading.rmd",
+    output_dir="/Users/ARC85/cillo_lab_materials/lab_manuals/dry_lab_manual/Cillo_Dry_Lab_Manual/static"
+)
